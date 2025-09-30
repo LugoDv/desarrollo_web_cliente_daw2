a0a9435209a82aa501ea42da2e79f876417c1e59
LugoDv <falugopersonal@gmail.com>
2025-09-30 10:12:15 +0200

    feat: add exercise 5

---
+++ Laboratorio_2/ejercicio_5/script.js
@@ -0,0 +1,54 @@
+class Alumno {
+  constructor(nombre, edad, nota) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.nota = nota;
+  }
+
+  estaAprobado() {
+    return this.nota >= 5;
+  }
+
+  presentacion() {
+    let aprobado = "";
+    if (this.estaAprobado()) {
+      aprobado = "(aprobado)";
+    } else {
+      aprobado = "(desaprobado)";
+    }
+    return (
+      "soy " +
+      this.nombre +
+      " tengo " +
+      this.edad +
+      " años " +
+      "y mi nota es " +
+      this.nota +
+      aprobado
+    );
+  }
+}
+
+class AlumnoBecado  extends Alumno{
+    
+
+    constructor(nambre, edad, nota, beca) {
+        super(nambre, edad, nota);
+        this.beca = beca;
+    }
+
+    presentacion(){
+        return super.presentacion() + " mi beca es de " + this.beca + "€";
+    }
+}
+
+alumno1 = new Alumno("fabian", 29, 4);
+alumno2 = new Alumno("joel", 39, 8);
+alumno3 = new Alumno("aron", 29, 10);
+
+
+alumnoBeca = new AlumnoBecado("Amanda",25,10,1200);
+console.log(alumno1.presentacion());
+console.log(alumno2.presentacion());
+console.log(alumno3.presentacion());
+console.log(alumnoBeca.presentacion());
