bd43167a5ff5e6fdd64bf7433f967e3bb18f0722
LugoDv <falugopersonal@gmail.com>
2026-03-14 22:34:57 +0100

    feat:add and delete task

---
+++ gestor_tareas/script.js
@@ -0,0 +1,50 @@
+let tasks = [{ id: 1, text: "Configurar nvm en fish init" }];
+
+const $taskList = document.getElementById("task-list");
+const $taskForm = document.getElementById("task-form");
+const $taskInput = document.getElementById("task-input");
+const $pendingCount = document.getElementById("pending-count");
+
+const renderTask = () => {
+  $taskList.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const $taskItem = document.createElement("li");
+    $taskItem.className = "task-item";
+    $taskItem.setAttribute("data-id", task.id);
+    $taskItem.textContent = task.text;
+
+    $taskItem.addEventListener("click", () => {
+      console.log(`${task.text}`);
+    });
+
+    const $deleteButton = document.createElement("button");
+    $deleteButton.className = "btn-add btn-delete";
+    $deleteButton.textContent = "DEL";
+
+    $deleteButton.addEventListener("click", (e) => {
+      e.stopPropagation();
+
+      $taskItem.remove();
+
+      tasks = tasks.filter((t) => t.id !== task.id);
+    });
+
+    $taskItem.appendChild($deleteButton);
+    $taskList.appendChild($taskItem);
+  });
+};
+
+$taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let text = $taskInput.value.trim();
+
+  if (text === "") return;
+
+  const newTask = { id: Date.now(), text: text, completed: false };
+  tasks.push(newTask);
+  $taskInput.value = "";
+  renderTask();
+});
+
+renderTask();
